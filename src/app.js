086d6db861a6a11357e70479c91a4932b61cadd8
Maxwell Masharia <viaxender@gmail.com>
2020-06-15 13:16:27 +0300

    Finished configuration

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
 import getIds from "./utils/getIds";
-console.log("Ids", getIds(20));
+const input = document.querySelector(`input[type="number"]`);
+const btn = document.querySelector("button");
+
+let val = 0;
+input.addEventListener("change", () => {
+  val = Math.abs(input.value);
+});
+
+btn.addEventListener("click", () => {
+  console.log("Ids", getIds(val));
+  input.value = 0;
+  val = 0;
+});
